6b89adecb3c94b44be7e2caf67e62b0ea28d011d
git-k <kgunu93@gmail.com>
2021-05-26 18:22:41 +0900

    Modify: eslintcache .gitignore 추가, Routes.js 코드 작성

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class Routes extends React.Component {
   render() {
     return (
-        <Router>
-          <Switch>
-          </Switch>
-        </Router>
+      <Router>
+        <Switch></Switch>
+      </Router>
     );
   }
 }
+
+export default Routes;
