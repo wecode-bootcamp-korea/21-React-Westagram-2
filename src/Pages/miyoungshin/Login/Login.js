416ba96f4c29a1083fa31498a11e6e0a90ea52d0
shinmiyoung <smy910102@gmail.com>
2021-05-28 16:59:26 +0900

    Add: 로그인, 메인 페이지 scss 구현 완료

--- src/Pages/miyoungshin/Login/Login.js
+++ src/Pages/miyoungshin/Login/Login.js
@@ -1,8 +1,83 @@
 import React from 'react';
+import './Login.scss';
 
 class LoginMi extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="loginMi">
+        <section>
+          <div className="mainImages">
+            <img
+              src="/images/miyoungshin/login/smartphone.png"
+              alt="smartphone"
+            />
+            <img
+              className="thumbnailImage"
+              src="/images/miyoungshin/login/rolling-1.jpeg"
+              alt="instagram thumbnail"
+            />
+          </div>
+        </section>
+        <section className="contents">
+          <article className="logInBox">
+            <h1>Westagram</h1>
+            <div className="idInput">
+              <input type="text" required />
+              <label>Phone number, username, or email</label>
+            </div>
+            <div className="pwInput">
+              <input type="password" />
+              <label>Password</label>
+              <button>Show</button>
+            </div>
+            <button className="logInButton" disabled="disabled">
+              Log in
+            </button>
+            <div className="or">
+              <div className="orLine"></div>
+              <div className="orText">OR</div>
+              <div className="orLine"></div>
+            </div>
+            <div className="logInFacebook">
+              <a href="#">
+                <img
+                  src="/images/miyoungshin/login/facebook.png"
+                  alt="facebook icon"
+                />
+                <p>Log in with Facebook</p>
+              </a>
+            </div>
+            <div className="forgotPassword">
+              <a href="#">Forgot password?</a>
+            </div>
+          </article>
+          <article className="signUpBox">
+            <p>
+              Don't have an account? <a href="#">Sign up</a>
+            </p>
+          </article>
+          <article className="getTheApp">
+            <p>Get the app.</p>
+          </article>
+          <article>
+            <a href="#">
+              <img
+                className="appDownloadButton"
+                src="/images/miyoungshin/login/appstore-button.png"
+                alt="app store button"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="appDownloadButton"
+                src="/images/miyoungshin/login/googleplay-button.png"
+                alt="googleplay button"
+              />
+            </a>
+          </article>
+        </section>
+      </div>
+    );
   }
 }
 
--- src/Pages/miyoungshin/Main/Main.js
+++ src/Pages/miyoungshin/Main/Main.js
@@ -1,8 +1,177 @@
 import React from 'react';
+import CommonNav from '../../../Components/Nav/Nav';
+import './Main.scss';
 
 class MainMi extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="MainMi">
+        <CommonNav />
+        {/*
+        <nav>
+          <div>
+            <h1 className="logo">Westagram</h1>
+            <div className="search">
+              <label className="searchLabel">
+                <i className="fas fa-search"></i>
+                <span className="searchLabelText">Search</span>
+              </label>
+              <input className="searchInput" type="text" />
+              <button className="searchDeleteButton">
+                <i className="fas fa-times-circle"></i>
+              </button>
+            </div>
+            <div className="navIcons">
+              <img
+                src="/images/miyoungshin/main/icon/home_black.png"
+                alt="home icon"
+              />
+              <img
+                src="/images/miyoungshin/main/icon/send.png"
+                alt="send icon"
+              />
+              <img
+                src="/images/miyoungshin/main/icon/compass.png"
+                alt="compass icon"
+              />
+              <img
+                src="/images/miyoungshin/main/icon/heart.png"
+                alt="heart icon"
+              />
+              <img
+                className="myProfile-img"
+                src="/images/miyoungshin/main/profile/my-profile.jpeg"
+                alt="my profile"
+              />
+            </div>
+          </div>
+        </nav>
+        */}
+        <section className="mainSection">
+          <article>
+            <div className="stories">
+              <div class="story">
+                <div class="storyBg">
+                  <img
+                    class="storyProfile"
+                    src="/images/miyoungshin/main/profile/dlwlrma-profile.jpeg"
+                    alt="dlwlrma profile"
+                  />
+                </div>
+                <div class="storyId">dlwlrma</div>
+              </div>
+            </div>
+            <div className="feeds">
+              <div className="feedHead">
+                <div className="account">
+                  <img
+                    src="/images/miyoungshin/main/profile/my-profile.jpeg"
+                    alt="my profile"
+                  />
+                  <div>my_sshin</div>
+                </div>
+                <div className="accountMore">・・・</div>
+              </div>
+              <div className="feedBody">
+                <img
+                  className="feedPicture"
+                  src="/images/miyoungshin/main/feed-image.JPG"
+                  alt="feed"
+                />
+                <div className="feedReaction">
+                  <img
+                    className="floatLeft"
+                    src="/images/miyoungshin/main/icon/heart.png"
+                    alt="heart icon"
+                  />
+                  <img
+                    className="floatLeft"
+                    src="/images/miyoungshin/main/icon/chat.png"
+                    alt="chat icon"
+                  />
+                  <img
+                    className="floatLeft"
+                    src="/images/miyoungshin/main/icon/send.png"
+                    alt="send icon"
+                  />
+                  <img
+                    className="floatRight"
+                    src="/images/miyoungshin/main/icon/bookmark_white.png"
+                    alt="bookmark icon"
+                  />
+                </div>
+                <div className="feedInfo">
+                  <div className="feedLikes">15 likes</div>
+                  <div className="feedContents">
+                    <span className="myAccountId">my_sshin</span> 그림같은 카페
+                  </div>
+                  <div className="feedCommentsList">
+                    <ul>
+                      <li>
+                        <span class="commentId">ninini </span>
+                        <span class="commentText">예쁘다!</span>
+                        <button class="commentDeleteButton">delete</button>
+                        <button>
+                          <img
+                            class="commentLikesButton"
+                            src="/images/miyoungshin/main/icon/heart.png"
+                            alt="heart icon"
+                          />
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="feedDate">42 MINUTES AGO</div>
+                </div>
+                <div className="feedComment">
+                  <img
+                    src="/images/miyoungshin/main/icon/smile.png"
+                    alt="emoticon button"
+                  />
+                  <input
+                    id="commentInput"
+                    type="text"
+                    placeholder="Add a comment..."
+                  />
+                  <button id="postButton" disabled="disabled">
+                    Post
+                  </button>
+                </div>
+              </div>
+            </div>
+          </article>
+          <aside>
+            <div className="myAccount">
+              <img
+                src="/images/miyoungshin/main/profile/my-profile.jpeg"
+                alt="my profile"
+              />
+              <div className="myAccount-id">my_sshin</div>
+              <button className="sideButton">Swicth</button>
+            </div>
+            <div className="suggestions">
+              <div className="suggestionTitle">
+                <div>Suggestions For You</div>
+                <button>See All</button>
+              </div>
+              <div className="suggestedLists">
+                <div class="suggestedUser">
+                  <img
+                    src="/images/miyoungshin/main/profile/jparkitrighthere-profile.jpeg"
+                    alt="suggested User's profile"
+                  />
+                  <div>
+                    <div class="suggestedId">jparkitrighthere</div>
+                    <div class="suggestedText">Suggested for you</div>
+                  </div>
+                  <button class="sideButton">Follow</button>
+                </div>
+              </div>
+            </div>
+          </aside>
+        </section>
+      </div>
+    );
   }
 }
 
