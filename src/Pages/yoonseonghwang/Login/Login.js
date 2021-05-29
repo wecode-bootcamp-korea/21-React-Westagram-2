fb27f3add7164d02286787e5ca04a87b7b7ec1b5
무제 <mint1480123@gmail.com>
2021-05-29 16:24:08 +0900

    Add: Mission1 사용자 입력 데이터 저장 기능구현

--- src/Pages/yoonseonghwang/Login/Login.js
+++ src/Pages/yoonseonghwang/Login/Login.js
@@ -1,8 +1,148 @@
 import React from 'react';
+import './Login.scss';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      backgroundColor: '#c0dffd',
+      disabled: true,
+      id: '',
+      pw: '',
+    };
+  }
+
+  handleIdInput = event => {
+    this.setState({
+      id: event.target.value,
+    });
+  };
+
+  handlePwInput = event => {
+    this.setState({
+      pw: event.target.value,
+    });
+
+    !this.state.id.includes('@') || this.state.pw.length < 5
+      ? this.setState({ disabled: true, backgroundColor: '#c0dffd' })
+      : this.setState({ disabled: false, backgroundColor: '#0095f6' });
+  };
+  goToMain = () => {
+    this.props.history.push('./Main');
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div className="total">
+        <div className="topContainer">
+          <div className="phone">
+            <img
+              className="dummyParent"
+              src="/images/yoonseonghwang/Login-Dummy/smartphone.png"
+              alt="phone"
+            />
+            <img
+              className="dummyChild"
+              src="/images/yoonseonghwang/Login-Dummy/rolling-1.jpeg"
+              alt="dummy"
+            />
+          </div>
+          <div className="contentsContainer">
+            <div className="loginPage">
+              <header>
+                <h1>Westagram</h1>
+              </header>
+              <main>
+                <div className="inputContainer">
+                  <form>
+                    <input
+                      type="text"
+                      className="id"
+                      placeholder="전화번호, 사용자 이름 또는 이메일"
+                      value={this.state.id}
+                      onChange={this.handleIdInput}
+                    />
+                    <input
+                      type="password"
+                      name="pw"
+                      className="password"
+                      placeholder="비밀번호"
+                      onChange={this.handlePwInput}
+                      value={this.state.pw}
+                    />
+                    <button
+                      type="submit"
+                      className="loginBtn"
+                      onClick={this.goToMain}
+                      style={{ backgroundColor: this.state.backgroundColor }}
+                      disabled={this.state.disabled}
+                    >
+                      로그인
+                    </button>
+                  </form>
+
+                  <div className="junkWrap">
+                    <div className="junkLine"></div>
+                    <div className="junkP">또는</div>
+                    <div className="junkLine"></div>
+                  </div>
+
+                  <div className="faceBook">
+                    <i className="fab fa-facebook-square"></i>
+                    <span>facebook으로 로그인</span>
+                  </div>
+                  <div className="secondWay">
+                    <a href="/" aria-label="find password">
+                      비밀번호를 잊으셨나요?
+                    </a>
+                  </div>
+                </div>
+              </main>
+            </div>
+            <aside className="joinContainer">
+              <div className="join">
+                <p>계정이 없으신가요</p>
+                <a href="/">가입하기</a>
+              </div>
+              <div className="induceUser">
+                <span>앱을 다운로드하세요.</span>
+                <div className="storeBtn">
+                  <img
+                    className="appStore"
+                    src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_korean-ko.png/f155b664a93b.png"
+                    alt="appstore"
+                  />
+                  <img
+                    className="googlePlay"
+                    src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_korean-ko.png/4a5c9d62d51b.png"
+                    alt="googleplay"
+                  />
+                </div>
+              </div>
+            </aside>
+          </div>
+        </div>
+
+        <footer>
+          <div className="footer1">
+            <span>소개</span>
+            <span>블로그</span>
+            <span>채용 정보</span>
+            <span>도움말</span>
+            <span>API</span>
+            <span>개인정보처리방침</span>
+            <span>약관</span>
+            <span>인기 계정</span>
+            <span>해시태그</span>
+            <span>위치</span>
+          </div>
+          <div className="footer2">
+            <span>한국어</span>
+            <span>© 2021 Instagram from Facebook</span>
+          </div>
+        </footer>
+      </div>
+    );
   }
 }
 
--- src/Pages/yoonseonghwang/Main/Main.js
+++ src/Pages/yoonseonghwang/Main/Main.js
@@ -1,8 +1,172 @@
 import React from 'react';
+import './Main.scss';
+import Nav from '../../../Components/Nav/Nav';
 
 class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
   render() {
-    return <div></div>;
+    return (
+      <div className="mainPage1">
+        <Nav />
+
+        <main>
+          <div className="feeds">
+            <article className="feed">
+              <div className="contents_container">
+                <div className="post_author">
+                  <img
+                    src="/images/yoonseonghwang/feed/myprofile.jpg"
+                    alt="my profile"
+                    className="author_profile"
+                  />
+                  <span>
+                    <b>yoon_in_verse</b>
+                  </span>
+                </div>
+
+                <div className="contents">
+                  <img
+                    src="/images/yoonseonghwang/feed/dogie.jpg"
+                    alt="feeds"
+                  />
+                </div>
+
+                <div className="active_container">
+                  <div className="comunicate_bar">
+                    <div className="c_bar_left">
+                      <button className="like">
+                        <img
+                          src="/images/yoonseonghwang/comment/heart.png"
+                          alt="like"
+                        />
+                      </button>
+                      <button>
+                        <img
+                          src="/images/yoonseonghwang/comment/comment.png"
+                          alt="comment"
+                        />
+                      </button>
+                      <button>
+                        <img
+                          src="/images/yoonseonghwang/comment/dm.png"
+                          alt="message"
+                        />
+                      </button>
+                    </div>
+                    <div className="c_bar_right">
+                      <button>
+                        <img
+                          src="/images/yoonseonghwang/comment/bookmark.png"
+                          alt="save this feed"
+                        />
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="counting_box">
+                    좋아요 <span className="like_count">11</span> 개
+                  </div>
+
+                  <div className="comment_container">
+                    <ul id="commentBox"></ul>
+                  </div>
+
+                  <div className="input-box">
+                    <input
+                      type="text"
+                      placeholder="댓글 달기..."
+                      className="input_comment"
+                      id="inputComment"
+                    />
+                    <button
+                      type="submit"
+                      className="comment_button"
+                      id="commentButton"
+                    >
+                      게시
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+
+          <section className="rec_container">
+            <div className="menu_title">
+              <span className="sub_title">회원님을 위한 추천</span>
+              <span className="more">모두 보기</span>
+            </div>
+            <ul className="recommend_list">
+              <li>
+                <div className="recommend_profile">
+                  <img
+                    className="img_profile"
+                    src="/images/yoonseonghwang/story/raonlee.jpg"
+                    alt="raonlee님의 프로필 사진"
+                  />
+                  <div className="profile_text">
+                    <span className="user_id">raonlee</span>
+                    <span className="sub_span">
+                      radmole님 외 11명이 팔로우합니다
+                    </span>
+                  </div>
+                </div>
+                <span className="follow">팔로우</span>
+              </li>
+              <li>
+                <div className="recommend_profile">
+                  <img
+                    className="img_profile"
+                    src="/images/yoonseonghwang/story/jmin.jpg"
+                    alt="raonlee님의 프로필 사진"
+                  />
+                  <div className="profile_text">
+                    <span className="user_id">shift_j__min2</span>
+                    <span className="sub_span">
+                      lejh066259님 외 3명이 팔로우합니다
+                    </span>
+                  </div>
+                </div>
+                <span className="follow">팔로우</span>
+              </li>
+              <li>
+                <div className="recommend_profile">
+                  <img
+                    className="img_profile"
+                    src="/images/yoonseonghwang/story/chaeyun.jpg"
+                    alt="eunbvy님의 프로필 사진"
+                  />
+                  <div className="profile_text">
+                    <span className="user_id">eunbvy</span>
+                    <span className="sub_span">회원님을 위한 추천</span>
+                  </div>
+                </div>
+                <span className="follow">팔로우</span>
+              </li>
+              <li>
+                <div className="recommend_profile">
+                  <img
+                    className="img_profile"
+                    src="/images/yoonseonghwang/story/junki.jpg"
+                    alt="jun_kiiiii님의 프로필 사진"
+                  />
+                  <div className="profile_text">
+                    <span className="user_id">jun_kiiiii</span>
+                    <span className="sub_span">
+                      dr.brrrrrrr님 외 4명이 팔로우합니다
+                    </span>
+                  </div>
+                </div>
+                <span className="follow">팔로우</span>
+              </li>
+            </ul>
+          </section>
+        </main>
+      </div>
+    );
   }
 }
 
